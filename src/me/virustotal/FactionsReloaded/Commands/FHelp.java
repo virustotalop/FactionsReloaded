ff225753c9858cfcc54f9c27514b52c1fa3babef
virustotalop <virustotalop@hushmail.com>
2015-04-19 20:09:33 -0700

    Revert "Revert "Work on faction tags mainly complete""
    This reverts commit 2736a72e0e98ec91460d1234df4640c8de197621.

--- src/me/virustotal/FactionsReloaded/Commands/FHelp.java
+++ src/me/virustotal/FactionsReloaded/Commands/FHelp.java
@@ -1,12 +1,14 @@
 package me.virustotal.FactionsReloaded.Commands;
 
+import org.bukkit.entity.Player;
+
 public class FHelp {
 
-	public static boolean doCommand()
+	public static boolean doCommand(Player player)
 	{
 		return true;
 	}
-	public static boolean doCommandWithArg(String arg)
+	public static boolean doCommandWithArg(Player player, String arg)
 	{
 		return true;
 	}
--- src/me/virustotal/FactionsReloaded/ConfigHandler.java
+++ src/me/virustotal/FactionsReloaded/ConfigHandler.java
@@ -36,7 +36,11 @@ public class ConfigHandler {
 	//Messages
 	public static String prefix;
 	public static String cannotClaimNoFaction;
+	public static String cannotDoFtagNoFaction;
+	public static String cannotDoFTagNotCorrectGroup;
 	public static String landClaimed;
+	public static String fTagNoArgs;
+	public static String fTagInUse;
 	
 	//Help Menu
 	public static List<String> topOfMenu;
@@ -80,7 +84,11 @@ public class ConfigHandler {
 	
 		ConfigHandler.prefix = plugin.tConfigString("prefix");
 		ConfigHandler.cannotClaimNoFaction = ConfigHandler.prefix + plugin.tConfigString("cannot-claim-no-faction");
+		ConfigHandler.cannotDoFtagNoFaction = ConfigHandler.prefix + plugin.tConfigString("cannot-do-ftag-no-faction");
+		ConfigHandler.cannotDoFTagNotCorrectGroup = ConfigHandler.prefix + plugin.tConfigString("cannot-do-ftag-not-correct-group");
 		ConfigHandler.landClaimed = ConfigHandler.prefix + plugin.tConfigString("land-claimed");
+		ConfigHandler.fTagNoArgs = ConfigHandler.prefix + plugin.tConfigString("ftag-no-args");
+		ConfigHandler.fTagInUse = ConfigHandler.prefix + plugin.tConfigString("ftag-in-use");
 		
 		/* Help Menu
 		 * 
@@ -125,7 +133,7 @@ public class ConfigHandler {
 			//save
 		}
 		
-		/*Load in factions
+		/*Load in factions, fwarps and fhomes          - Do optimizations to storage later
 		 *Populates the list in the main factions class
 		 *Creates a new object for each faction
 		 */
@@ -140,8 +148,10 @@ public class ConfigHandler {
 			int power = factionsConfig.getInt(name + ".power");
 			int land = factionsConfig.getInt(name + ".land");
 			boolean open = factionsConfig.getBoolean(name + ".open");
-			List<String> members = factionsConfig.getStringList(name + ".members");
-			Faction fac = new Faction(name,tag,power,land,open,(ArrayList<String>) members);
+			ArrayList<String> members = (ArrayList<String>) factionsConfig.getStringList(name + ".members");
+			String admin = factionsConfig.getString(name + ".admin");
+			ArrayList<String> mods = (ArrayList<String>) factionsConfig.getStringList(name + ".mods");
+			Faction fac = new Faction(name,tag,power,land,open,admin,mods, members);
 			String[] locSplit = factionsConfig.getString(name + ".fhome.location").split(",");
 			int x = Integer.parseInt(locSplit[0]);
 			int y = Integer.parseInt(locSplit[1]);
@@ -176,8 +186,6 @@ public class ConfigHandler {
 				
 				plugin.fWarps.add(new FWarp(fac,wLoc,warpName,group,password));
 			}
-			
-			
 		}
 		
 		/*Loads in entirety of the board
@@ -199,8 +207,6 @@ public class ConfigHandler {
 		{
 			plugin.fPlayers.add(new FPlayer(UUID.fromString(string),playerConfig.getInt(string + ".power"),playerConfig.getString(string + ".faction")));
 		}
-		
-		
 		
 	}
 
